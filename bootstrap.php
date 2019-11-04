fad653d1d2f54f7ba439ef94487369fd65bc9379
izytechAB <thomas@izytech.se>
2019-11-04 14:25:44 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,9 +2,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Entity.php';
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Auto.php';
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Property.php';
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/Index.php';
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/ManyToOne.php';
-require __DIR__ . '/lib/HireVoice/Neo4j/Annotation/ManyToMany.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/Entity.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/Auto.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/Property.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/Index.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/ManyToOne.php';
+require __DIR__ . '/lib/izytechAB/Neo4j/Annotation/ManyToMany.php';
